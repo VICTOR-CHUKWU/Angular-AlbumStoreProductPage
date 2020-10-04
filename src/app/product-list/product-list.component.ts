fc698f51f4b0d177d3a01958a67f5c3db77aa759
victor-chukwu <okenwa1993@gmail.com>
2020-10-04 20:49:16 +0100

    my sixt second commit

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Product } from '../product';
+
 import { ProductService } from '../product.service';
 
 @Component({
@@ -10,7 +12,7 @@ import { ProductService } from '../product.service';
 
 
 export class ProductListComponent implements OnInit {
-  products = Product[]
+  products : Product[];
 
   constructor(private _productService: ProductService) { }
 
